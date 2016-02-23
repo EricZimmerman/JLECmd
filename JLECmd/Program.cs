1d2f63e5b994a94a9e42f0f53a7ca3ffde315de4
Eric Zimmerman <saericzimmerman@gnail.com>
2016-02-23 12:22:20 -0700

    Add project files.

---
+++ JLECmd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLECmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
